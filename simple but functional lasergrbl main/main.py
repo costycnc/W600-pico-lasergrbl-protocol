cc9cdc9271d6e3086c068c4a62afc3921fe84d79
Boboaca Costel <bobyca2003@yahoo.com>
2023-03-02 16:14:44 +0100

    Update main.py

--- simple but functional lasergrbl main/main.py
+++ simple but functional lasergrbl main/main.py
@@ -5,25 +5,22 @@ s.bind(('', 23))
 s.listen(5)
 conn, addr = s.accept()
 #conn.setblocking(False)
-
-x=0
  
-while True:            
-    try:
-        request = conn.recv(200)                
-        if request:                
-            x +=1
-            print("\r")
-            print(x)             
+while True:           
+    request = conn.recv(200)                
+    if request:                
+        x +=1
+        print("\r")
+        print(x)             
+        print(request)
+        conn.send("ok\n")  
+        if "?" in request:   
+            conn.send("<Idle|MPos:0.000,0.000,0.000|FS:0,0>\r")
+        else:      
+            print("\r")        
             print(request)
             conn.send("ok\n")  
-            if "?" in request:   
-                conn.send("<Idle|MPos:0.000,0.000,0.000|FS:0,0>\r")
-                x -=1            
-    except Exception as e:
-        #print('Exception: ', e)
-        #print('no data from', addr)
-        time.sleep(1)       
+      
 # Clean up the connection.
 conn.close()
 print("closed. ") 
